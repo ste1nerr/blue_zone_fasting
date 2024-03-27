410eadbf6cea5bcf12d881cd6b5daa318af0b889
ste1nerr <99362390+ste1nerr@users.noreply.github.com>
2024-03-27 10:39:46 +0200

    commit

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -54,59 +54,64 @@ const styles = {
   },
   buttonContainer: {
     textAlign: 'center',
-    marginTop: '34px',
-  },
-  button: {
-    padding: '10px 100px',
-    borderRadius: "50px",
   },
   appBarBottom: {
     display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
     boxShadow: 'none',
     height: '100px',
     backgroundColor: 'transparent',
     borderTop: '1px solid #ccc',
-},
+  },
+  button: {
+    backgroundColor: '#1488F0',
+    borderRadius: '50px',
+    width: '350px',
+    height: '50px',
+    padding: '10px 16px',
+    textTransform: 'none',
+    fontWeight: '700'
+  },
 };
 
 function MainPage() {
-    return (
-        <ThemeProvider theme={theme}>
-            <div style={styles.container}>
-                <AppBar position="static" sx={styles.appBar}>
-                    <Toolbar>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
-                            <Image src="/images/logo.svg" alt="Company Logo" width={250} height={50} />
-                        </Typography>
-                        <Image src="/images/bars.svg" alt="Quiz Image" width={32} height={32} />
-                    </Toolbar>
-                </AppBar>
+  return (
+    <ThemeProvider theme={theme}>
+      <div style={styles.container}>
+        <AppBar position="static" sx={styles.appBar}>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
+              <Image src="/images/logo.svg" alt="Company Logo" width={250} height={50} />
+            </Typography>
+            <Image src="/images/bars.svg" alt="Quiz Image" width={32} height={32} />
+          </Toolbar>
+        </AppBar>
 
-                <Container sx={styles.mainContainer}>
-                    <Typography variant="h4" sx={styles.title}>
-                        Begin Your Intermittent Fasting Program
-                    </Typography>
-                    <Image src="/images/before&after.svg" alt="Quiz Image" width={500} height={400} />
-                </Container>
+        <Container sx={styles.mainContainer}>
+          <Typography variant="h4" sx={styles.title}>
+            Begin Your Intermittent Fasting Program
+          </Typography>
+          <Image src="/images/before&after.svg" alt="Quiz Image" width={500} height={400} />
+        </Container>
 
-                <Container sx={styles.descriptionContainer}>
-                    <Typography variant="subtitle1" sx={styles.description}>
-                        Unlock your personalised fasting plan by answering a few quick questions
-                    </Typography>
-                </Container>
+        <Container sx={styles.descriptionContainer}>
+          <Typography variant="subtitle1" sx={styles.description}>
+            Unlock your personalised fasting plan by answering a few quick questions
+          </Typography>
+        </Container>
 
 
-                <AppBar position="static" sx={styles.appBarBottom}>
-                    <Container sx={styles.buttonContainer}>
-                        <Link href="/testPage">
-                            <Button variant="contained" color="primary" sx={styles.button}>Continue</Button>
-                        </Link>
-                    </Container>
-                </AppBar>
-            </div>
-        </ThemeProvider>
-    );
+        <AppBar position="static" sx={styles.appBarBottom}>
+          <Container sx={styles.buttonContainer}>
+            <Link href="/testPage">
+              <Button variant="contained" color="primary" sx={styles.button}>Continue</Button>
+            </Link>
+          </Container>
+        </AppBar>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default MainPage;
