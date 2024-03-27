7808c166f2f8d227a487d1853e832bc84974f9e0
ste1nerr <99362390+ste1nerr@users.noreply.github.com>
2024-03-27 05:05:49 +0200

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import MainPage from './MainPage';
+import MainPage from '@/pages/mainPage';
 
 
 export default function Home() {
